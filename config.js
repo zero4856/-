a30f8aa5c7d910937283407750e694b3761ed8da
zero4856 <a01055385014@gmail.com>
2026-07-13 21:25:46 +0900

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.ZERO_CONFIG = {
-  SUPABASE_URL: "https://ydcxumqyuxgveljisdzt.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://ydcxumqyuxgveljisdzt.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_1Dh85xEJDiYSVAt0CNmznw_jnumZSlr"
 };
